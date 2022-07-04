5010a48ac40181af93e46ac10704c74ad6254843
Bastian Matteo <matteocarvajal.eth@gmail.com>
2022-07-04 01:15:12 -0400

    Se agrega variables enteras

---
+++ Tipos de variables y operadores/variables_enteras.sol
@@ -0,0 +1,8 @@
+pragma solidity 0.8.15; // Indicamos la versión de solidity que vamos a usar
+ 
+contract enteros{
+    // Variables enteras sin signo
+    uint mi_primer_entero; // Declaración de la variable entera sin signo
+    uint mi_primer_entero_inicializado = 3; // Declaración de la variable entera sin signo inicializada
+
+}
